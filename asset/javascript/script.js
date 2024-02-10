879113e9de1fc638a3385fa55e7c9b35a640066d
bertellimatteo <lavoro.bertellimatteo@gmail.com>
2024-02-10 15:50:36 +0100

    formattin js

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -4,23 +4,22 @@ function calcoloPrezzo() {
   const km = parseFloat(document.getElementById('km').value);
   const age = parseInt(document.getElementById('age').value);
   const fullName = document.getElementById('fullName').value; 
-
-  document.getElementById('passengerName').textContent = fullName;
-
+  
   const basePrice = km * 0.21;
-
+  
   if (age < 18) {
     basePrice *= 0.8; 
   } 
-
+  
   else if (age >= 65) {
     basePrice *= 0.6; 
   }
-
+  
   const finalPrice = basePrice.toFixed(2);
-
+  
   document.getElementById('ticketPrice').textContent = finalPrice + "€";
-
+  
+  document.getElementById('passengerName').textContent = fullName;
 }
 
 console.log(calcoloPrezzo())
